88114ed04bc086180e006711bb7c1d29d6a98d8e
Jackson Morgan <jmorgan45@gatech.edu>
2016-02-12 21:19:47 -0500

    New site

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(express.static('./public'));
+
+var port = process.env.PORT || 8001;
+app.listen(port);
+console.log('Application listening on port ' + port);
